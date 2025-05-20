61a42be60c79864061237b6da872a3263caad725
Rugehoe <Rugehoe@hotmail.com>
2025-05-20 17:04:01 +0200

    camera done

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,10 +4,15 @@ public class CameraControl : MonoBehaviour
 {
     [SerializeField]
     GameObject player;
+    [SerializeField]
+    float speed = 3.0f;
+    [SerializeField]
+    float[] offset = new float[3];
 
     private void FixedUpdate()
     {
-        this.transform.position = new Vector3(player.transform.position.x,player.transform.position.y-4.5f, -6);
+        Vector3 playerpoint = new Vector3(player.transform.position.x + offset[0], player.transform.position.y + offset[1], player.transform.position.z + offset[2]); 
+        this.transform.Translate((playerpoint - this.transform.position) * speed * Time.deltaTime); 
 
     }
 }
